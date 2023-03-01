f834a9b84e26dcd4b270a8a475fff4b499d12a26
Cesar Hinojosa <112355092+CH2080@users.noreply.github.com>
2023-03-01 12:28:56 -0600

    add user route

--- e_commerce_be/index.js
+++ e_commerce_be/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import productRouter from "./Route/Product.js"
+import userInfor from "./Route/User.js"
 const app = express()
 
 app.use(express.json())
 
 app.use("/api/product", productRouter)
+app.use("/api/user", userInfor)
 
 app.listen(8800,()=>{
-    console.log('connected112')
-    console.log('hi')
+    console.log('connected')
 })

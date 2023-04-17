d84ce72e27d1755c4c93a44bc4e370a870af1585
khoaguye <Knguyenn72@gmail.com>
2023-04-17 10:34:54 -0500

    change

--- e_commerce_FE/src/Pages/Pages.jsx
+++ e_commerce_FE/src/Pages/Pages.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from 'react'
 import {ContextProvider} from "../Components/Product/ProductContext"
 import Home from "./Home"
@@ -38,35 +37,4 @@ function Pages() {
     </ContextProvider>
   )
 }
-
-=======
-import React from 'react'
-import {ContextProvider} from "../Components/Product/ProductContext"
-import Home from "./Home"
-import {Routes, Route} from 'react-router-dom'
-import Product from './Product'
-import Detail_product from './Detail_product'
-import Cart from './Cart'
-import Filter_bar from '../Components/Product/Filter_bar'
-import Product_manage from '../Components/Admin/Product_manage';
-import { Login } from '../Components/Login/Login'
-import { Register } from '../Components/Register/Register'
-import Admin from './Admin'
-function Pages() {
-  return (
-    <ContextProvider>
-    <Routes>
-        <Route path = "/" element={<Home/>}/>
-        <Route path = "/product" element={<Product/>}/>
-        <Route path ='/product/:id' element ={<Detail_product/>}/>
-        <Route path = '/cart' element={<Cart/>}/>
-        <Route path = '/admin' element={<Admin/>}/> 
-        <Route path = '/user/login' element={<Login/>}/>
-        <Route path = '/user/register' element={<Register/>}/>
-    </Routes>
-    </ContextProvider>
-  )
-}
-
->>>>>>> Stashed changes
 export default Pages
